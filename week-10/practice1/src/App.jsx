b15a1c5b9b944033126fc7d56c35aad793ddd16c
KayTee1 <kha.truong@tuni.fi>
2023-10-28 21:54:12 +0300

    worked on p1

--- week-10/practice1/src/App.jsx
+++ week-10/practice1/src/App.jsx
@@ -1,51 +1,45 @@
-import ProductList from './ProductList';
-
-import './App.css'
-
-const PRODUCTS = [
-  {
-    "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    "price": 109.95,
-    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "rating": {
-      "rate": 3.9,
-      "count": 120
-    }
-  },
-  {
-    "id": 2,
-    "title": "Mens Casual Premium Slim Fit T-Shirts ",
-    "price": 22.3,
-    "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-    "rating": {
-      "rate": 4.1,
-      "count": 259
-    }
-  },
-  {
-    "id": 3,
-    "title": "Mens Cotton Jacket",
-    "price": 55.99,
-    "description": "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-    "rating": {
-      "rate": 4.7,
-      "count": 500
-    }
-  }
-];
+import ProductList from "./ProductList";
+import axios from "axios";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const fetchProducts = () => {
-  
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchProducts = async () => {
+    try {
+      setError(null);
+      setIsLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error.message);
+      console.error(error);
+    }
+  };
+
+  let content = <p>No product data found</p>;
+
+  if (error) {
+    content = <p>{error}</p>;
   }
 
+  if (isLoading) {
+    content = <p>Fetching data... </p>;
+  }
+
+  if (products.length > 0) {
+    content = <ProductList products={products} />;
+  }
   return (
     <>
       <h1>Incorporating data to React</h1>
@@ -53,10 +47,10 @@ function App() {
         <button onClick={fetchProducts}>Fetch Products</button>
       </div>
       <div className="card">
-        <ProductList products={PRODUCTS}/>
+        {content}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
